3f0e9385a063afa3fc710d459629de42b84c0e4e
Jackie Sze <jackiesze0@gmail.com>
2020-02-29 02:33:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   // write your code here
   for (i = 0; i > cart.length; i++) {
-    if (cardNumber === undefined) {
+    if (typeOf(cardNumber === undefined) {
       return `Sorry, we don't have a credit card on file for you.`
     } else {
   }
