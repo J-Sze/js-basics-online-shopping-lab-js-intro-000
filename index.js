1fb1dc77b723dead47a2e599a70c7b70d64a0a41
Jackie Sze <jackiesze0@gmail.com>
2020-02-29 00:47:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function addToCart(item) {
 function viewCart() {
   // write your code here
   if (cart.length === 0) {
-    return `your shopping cart is empty.`
+    return `Your shopping cart is empty.`
   } else if (cart.length === 1) {
     return `In your cart, you have ${cart[0].itemName} at ${cart[0].itemPrice}.`
   } else if (cart.length === 2) {
