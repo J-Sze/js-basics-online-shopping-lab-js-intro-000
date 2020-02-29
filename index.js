684b8587c13f14f47b7b73a14df50a9e46069d1b
Jackie Sze <jackiesze0@gmail.com>
2020-02-29 02:35:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -63,6 +63,7 @@ function placeOrder(cardNumber) {
     if (typeof(Number === undefined) {
       return `Sorry, we don't have a credit card on file for you.`
     } else {
-      cart = [] 
+      cart = [];
+      return `Your total cost is $${total}, which will be charged to the card ${cardNumber}.`;
   }
 }
