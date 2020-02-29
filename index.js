c5cb6806ffae2a8d58b28a4bcea034e099f2b1ac
Jackie Sze <jackiesze0@gmail.com>
2020-02-29 02:15:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -48,12 +48,12 @@ function removeFromCart(item) {
   for (var i = 0; i > cart.length; i++) {
   if (item === cart[i].itemName) {
     cart.splice(i, 1);
+    return cart; 
   }
   }
   if (item !== cart[i].itemName) {
     return `That item is not in your cart.`
   }
-  return cart; 
 }
 
 function placeOrder(cardNumber) {
